5357dd390ebefd8791496be0a14f9bc204a6105a
Sankalp Jaiswal <sankalpjaiswal95@gmail.com>
2025-04-09 19:15:34 +0530

    trial commit

--- client/App.js
+++ client/App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Hi, I am Sankalp Jaiswal</Text>
+      <Text>Hi</Text>
       <StatusBar style="auto" />
     </View>
   );
